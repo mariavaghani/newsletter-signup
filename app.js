f206695422cd00d99f2a1d8370a2e6d7f377189d
maria.vaghani <maria.vaghani@gmail.com>
2021-07-27 23:31:36 -0400

    added semi-colon

--- app.js
+++ app.js
@@ -15,68 +15,61 @@ const listId = "a85d497b3f";
 client.setConfig({
     apiKey: apiKey,
     server: server,
-  });
-
-  
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname+"/signup.html")
 });
 
-app.post("/", function(req,res) {
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", function (req, res) {
     console.log(req.body)
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var email = req.body.email;
     var subscriberHash = md5(email.toLowerCase());
-    
+
 
     const subscribingUser = {
         firstName: firstName,
         lastName: lastName,
         email: email
-      };
- 
-
-    async function run() {
-    const response = await client.lists.setListMember(
-        listId,
-        subscriberHash,
-        { email_address: subscribingUser.email,
-        status_if_new: "subscribed",
-        merge_fields: {
-            FNAME: subscribingUser.firstName,
-            LNAME: subscribingUser.lastName
-        }}
-    );
-    console.log(`Successfully added contact as an audience member. The contact's id is ${
-        response.id}.`);
-
-    console.log(response);
-    res.sendFile(__dirname+"/success.html")
     };
 
-    run().catch(e => { 
-        console.log(e.status)
-        res.sendFile(__dirname+"/failure.html") 
+
+    async function run() {
+        const response = await client.lists.setListMember(
+            listId,
+            subscriberHash, {
+                email_address: subscribingUser.email,
+                status_if_new: "subscribed",
+                merge_fields: {
+                    FNAME: subscribingUser.firstName,
+                    LNAME: subscribingUser.lastName
+                }
+            }
+        );
+        console.log(`Successfully added contact as an audience member. The contact's id is ${
+        response.id}.`);
+
+        console.log(response);
+        res.sendFile(__dirname + "/success.html");
+    };
+
+    run().catch(e => {
+        console.log(e.status);
+        res.sendFile(__dirname + "/failure.html");
     })
 });
 
-app.post("/failure", function(req, res) {
-    res.redirect("/")
+app.post("/failure", function (req, res) {
+    res.redirect("/");
 })
 app.listen(process.env.PORT || port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  });
-
-
-
-
-
-
-
-
-
-  
+    console.log(`Example app listening at http://localhost:${port}`);
+});
